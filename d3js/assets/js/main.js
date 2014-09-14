00cdcf77dc2d0c93336481cc47e326983a1ce63b
fabiantheblind <icke@fabianmoronzirfas.me>
2014-09-14 13:07:58 +0200

    added d3

---
+++ d3js/assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var data = [4, 8, 15, 16, 23, 42];
+  d3.select("#chart")
+    .selectAll("div")
+    .data(data)
+    .enter().append("div").attr("class","bar")
+    .style("width", function(d) {
+      return d * 10 + "px";
+    })
+    .text(function(d) {
+      return d;
+    });
+});
