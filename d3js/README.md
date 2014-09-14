1622cbbc0337d321ce9f958323a75e3987816819
fabiantheblind <icke@fabianmoronzirfas.me>
2014-09-14 15:42:25 +0200

    minor

---
+++ d3js/README.md
@@ -0,0 +1,4 @@
+This is a starter for D3.  
+D3 is the motherload of data graphics.  
+
+See [this](http://bl.ocks.org/) or [that](http://christopheviau.com/d3list/index.html) or [maybe this](http://bl.ocks.org/mbostock) or [also that](https://www.biostat.wisc.edu/~kbroman/D3/) or [do that](http://lmgtfy.com/?q=d3+tutorials).  
--- d3js/assets/js/main.js
+++ d3js/assets/js/main.js
@@ -1,13 +1,42 @@
 $(document).ready(function() {
   var data = [4, 8, 15, 16, 23, 42];
+
+  // the chart
+  // see http://alignedleft.com/tutorials/d3/the-power-of-data
   d3.select("#chart")
     .selectAll("div")
     .data(data)
-    .enter().append("div").attr("class","bar")
+    .enter().append("div").attr("class", "bar")
     .style("width", function(d) {
       return d * 10 + "px";
     })
     .text(function(d) {
       return d;
     });
+
+  // scatter plot
+  // see http://alignedleft.com/tutorials/d3/making-a-scatterplot
+  data = [
+    [5, 22],
+    [100, 32],
+    [20, 300],
+    [59, 230]
+  ];
+
+  var svg = d3.select("#plot")
+  .append('svg')
+  .attr("width", 500)
+  .attr('height', 100);
+
+  svg.selectAll("circle")
+  .data(data)
+  .enter()
+  .append("circle")
+  .attr('cx', function(d, i) {
+    return d[1];
+
+  }).attr('cy', function(d) {
+    return d[0];
+  }).attr('r', 5);
+
 });
